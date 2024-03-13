9e4a878b795d4410c8c130136bf5100c1480df68
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-03-13 23:28:47 +0900

    test

--- frontend/src/screens/PostArea.tsx
+++ frontend/src/screens/PostArea.tsx
@@ -8,15 +8,15 @@ export const PostArea = () => {
     const [inputTerm, setInputTerm] = useState('')
     const [inputRemark, setInputRemark] = useState('')
 
-    function sendInfo () {
-           userRepository.submit(
-               {
-                   name: inputName,
-                   nickName: inputNickName,
-                   term: inputTerm,
-                   remark: inputRemark
-               }
-           )
+    function sendInfo() {
+        userRepository.submit(
+            {
+                name: inputName,
+                nickName: inputNickName,
+                term: inputTerm,
+                remark: inputRemark
+            }
+        )
         setInputName('')
         setInputNickName('')
         setInputTerm('')
@@ -28,12 +28,13 @@ export const PostArea = () => {
             <div>名前</div>
             <input onChange={event => setInputName(event.target.value)} type="text" id='name' value={inputName}/>
             <div>ニックネーム</div>
-            <input onChange={event => setInputNickName(event.target.value)} type="text" id='nickName' value={inputNickName}/>
+            <input onChange={event => setInputNickName(event.target.value)} type="text" id='nickName'
+                   value={inputNickName}/>
             <div>FND</div>
             <input onChange={event => setInputTerm(event.target.value)} type="text" id='term' value={inputTerm}/>
             <div>一言</div>
             <input onChange={event => setInputRemark(event.target.value)} type="text" id='remark' value={inputRemark}/>
             <button onClick={sendInfo}>投稿</button>
-    </>)
+        </>)
 }
 
